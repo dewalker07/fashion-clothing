a3212c7baeab4285fb182844f05a137f8cf1677c
dewalker07 <ddn.dabbara@gmail.com>
2019-08-13 12:37:15 +0530

    Updates

---
+++ precache-manifest.c1599e30223ce9bd3df4c2b4f4af3b5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcad1fda00af42f9bb8a5dae26b57d96",
+    "url": "/fashion-clothing/index.html"
+  },
+  {
+    "revision": "c4d85cbbcdf821b79907",
+    "url": "/fashion-clothing/static/css/main.0bfef54b.chunk.css"
+  },
+  {
+    "revision": "1990de91354abac112ea",
+    "url": "/fashion-clothing/static/js/2.d9b928d8.chunk.js"
+  },
+  {
+    "revision": "c4d85cbbcdf821b79907",
+    "url": "/fashion-clothing/static/js/main.06e83943.chunk.js"
+  },
+  {
+    "revision": "f71369946fddeef580ea",
+    "url": "/fashion-clothing/static/js/runtime~main.45cff1d9.js"
+  }
+]);
