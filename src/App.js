d8c4e5c44b2baf5cc03807852672fe4d789cfa4b
Dabbara Diwakar Naidu <ddn.dabbara@gmail.com>
2019-08-14 15:03:54 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component.jsx';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+
 
 class App extends React.Component {
   render () {
