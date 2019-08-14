68a37ed48d8e8f4c07961471114dae6df324abca
Dabbara Diwakar Naidu <ddn.dabbara@gmail.com>
2019-08-14 14:57:48 +0530

    updated homepage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 
