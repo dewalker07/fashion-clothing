cfaa163d970cc679aa0ae89be46cae27bb094da5
Dabbara Diwakar Naidu <ddn.dabbara@gmail.com>
2019-08-16 18:32:25 +0530

    finishing sign in handle submit method

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ class App extends React.Component {
                 const userRef = await createUserProfileDocument(userAuth);
                 userRef.onSnapshot(snapShot => {
                     this.setState({
-                        currentUser: {
-                            id: snapShot.id,
-                            ...snapShot.data()
-                        }
-                    })
-                    console.log(this.state);
+                            currentUser: {
+                                id: snapShot.id,
+                                ...snapShot.data()
+                            }
+                        })
+                        // console.log(this.state);
                 })
             }
             this.setState({ currentUser: userAuth });
