bdd00ea2d2530c31255fcc0e65408e1cbab522ab
Dabbara Diwakar Naidu <ddn.dabbara@gmail.com>
2019-08-21 18:31:24 +0530

    added cart-icon and cart-dropdown components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 
 import './header.styles.scss';
 
@@ -29,7 +32,9 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
