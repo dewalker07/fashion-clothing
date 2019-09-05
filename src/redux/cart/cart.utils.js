1fd722e465a7120d2c29d25d781a65d90d36ae78
Dabbara Diwakar Naidu <ddn.dabbara@gmail.com>
2019-09-05 18:16:34 +0530

    updated cart-dropdown component with cart-items

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,14 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id = cartItemToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id
     );
+
     if (existingCartItem) {
         return cartItems.map(cartItem =>
-            cartItem.id = cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1 } :
+            cartItem.id === cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1 } :
             cartItem
-        )
+        );
     }
-    return [...cartItems, {...cartItemToAdd, quantity: 1 }]
-}
+
+    return [...cartItems, {...cartItemToAdd, quantity: 1 }];
+};
